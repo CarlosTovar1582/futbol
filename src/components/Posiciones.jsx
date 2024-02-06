19abd825a16e481cd6a6821668dc6315fd5aa22a
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2024-02-06 10:39:42 -0500

    Update Posiciones.jsx

--- src/components/Posiciones.jsx
+++ src/components/Posiciones.jsx
@@ -130,74 +130,58 @@ export default function Posiciones() {
                 <div className='rounded-2xl  h-6 bg-slate-600 text-sm'>Equipo</div>
                 <div className='rounded-2xl  h-6 bg-slate-600 text-sm'>Jugador</div> 
                 <div className='rounded-2xl  h-6 bg-slate-600 text-sm'>Goles</div>
+
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>1</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Maquina del mal</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>William Yucra</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>2</div> 
+                <div className='text-black rounded-2xl  h-6 text-sm'>Olivar City</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Cesar Espinoza</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>6</div>
+              
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>2</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Maquina del mal</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Leonel Quispe</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>2</div>    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Sport Electric</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Emerson Chilon</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>5</div>  
+              
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>3</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Olivar city</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Cesar Porcela</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>2</div>    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Santa Elvira/div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Robert Inca</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>4</div>    
+
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>4</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Olivar city</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Marcos Palomino</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>2</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Olivar City</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Paolo Espino</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>4</div> 
+
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>5</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Sport Verbenas</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Oliver Ostos</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>2</div>  
+                <div className='text-black rounded-2xl  h-6 text-sm'>Santa Elvira</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Dicson Mory</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>4</div>
+                  
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>6</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Sport Electrik</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Gian de la Cruz</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>La Maquina del Mal</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Leonel Quispe</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>3</div>   
+                  
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>7</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Olivar city</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Paolo Fonseca</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Los Potrillos</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Jair Rojas</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>3</div>
+                  
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>8</div>
                 <div className='text-black rounded-2xl  h-6 text-sm'>La Familia</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Gerardo Jamanca</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>Juan Carlos del Aguila</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>3</div>
+
+                  
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>9</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>La Familia</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Alexandro Villa.</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Olivar city</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Marcos Palomino</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>3</div>
+                           
+                  
                 <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>10</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Sport Verbenas</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Piero Armijo</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>
-                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>11</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Los potrillos</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Jair Rojas</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>
-                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>12</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Los potrillos</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Manuel Santos</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>  
-                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>13</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Los intratables</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Jean Carlos A.</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
-                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>14</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Los engreídos</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Jarol Ruiz</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>
-                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>15</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Los engreídos</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Jofre Morales</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
-                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>16</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Santa elvira</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Dicson Mory</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>
-                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>17</div>
-                <div className='text-black rounded-2xl  h-6 text-sm'>Santa elvira</div>                    
-                <div className='text-black rounded-2xl  h-6 text-sm'>Alex Mory</div>   
-                <div className='text-black rounded-2xl  h-6 text-sm'>1</div>                
+                <div className='text-black rounded-2xl  h-6 text-sm'>La Familia</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Gerardo Jamanca</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>3</div>    
                 
            
             </div>
@@ -224,7 +208,35 @@ export default function Posiciones() {
                 <div className='text-black rounded-2xl  h-6 text-sm'>Jeam  Pierrre Colan</div>   
                 <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
                 <div className='text-black rounded-2xl  h-6 text-sm'>0</div> 
-                
+              <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>3</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Santa Elvira</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Alex Mory</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
+                <div className='text-black rounded-2xl  h-6 text-sm'>0</div> 
+
+               <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>4</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Sport Electric</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Jean Carlos Chavez</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>0</div> 
+                <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
+
+               <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>5</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Sport Verbenas</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Alonzo  Huaman</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>0</div> 
+                <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
+
+                <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>6</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Los Potrillos</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Jair Rojas</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>0</div> 
+                <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
+
+                    <div className='text-black rounded-2xl border-2 border-slate-900 ... h-6 text-sm'>7</div>
+                <div className='text-black rounded-2xl  h-6 text-sm'>Santa Elvira</div>                    
+                <div className='text-black rounded-2xl  h-6 text-sm'>Dario Ortiz</div>   
+                <div className='text-black rounded-2xl  h-6 text-sm'>0</div> 
+                <div className='text-black rounded-2xl  h-6 text-sm'>1</div> 
            
             </div>  
          
